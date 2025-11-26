41282babd1446c852afea58f5d0690466204f343
Oscar <oscar.arantegui@gmail.com>
2025-11-26 18:34:50 +0100

    Module 15 basic routing

--- ai-tutor/src/app/app.routes.ts
+++ ai-tutor/src/app/app.routes.ts
@@ -5,6 +5,19 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.dev/license
  */
-import {Routes} from '@angular/router';
+import { Routes } from '@angular/router';
+import { RecipeList } from './recipe-list/recipe-list';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'recipes',
+        pathMatch: 'full'
+
+    },
+    {
+        path: '',
+        component: RecipeList
+    }
+];
--- ai-tutor/src/app/app.ts
+++ ai-tutor/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RecipeList } from './recipe-list/recipe-list'; // Import the new component
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RecipeList], // Add it to imports
-  template: '<app-recipe-list />',     // Use it in the template
+  imports: [RouterOutlet], // Add it to imports
+  template: '<router-outlet />',     // Use it in the template
   styleUrl: './app.css',
 })
 export class App { }
