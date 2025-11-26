025b58a989643ecefa6755a86f2e134ccb052740
Oscar <oscar.arantegui@gmail.com>
2025-11-26 19:31:25 +0100

    module 16

--- ai-tutor/src/app/recipe-list/recipe-list.ts
+++ ai-tutor/src/app/recipe-list/recipe-list.ts
@@ -3,12 +3,13 @@ import { RecipeModel } from '../models';
 import { RecipeDetail } from '../recipe-detail/recipe-detail';
 import { FormsModule } from '@angular/forms';
 import { RecipeService } from '../recipe.service';
+import { Router, RouterLink } from '@angular/router'
 
 @Component({
     selector: 'app-recipe-list',
     templateUrl: './recipe-list.html',
     styleUrl: './recipe-list.css',
-    imports: [RecipeDetail, FormsModule]
+    imports: [RecipeDetail, FormsModule, RouterLink]
 })
 export class RecipeList {
     // This component holds the "State"
